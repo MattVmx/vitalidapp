cbbabdc21c637b5d9d420cc93ab506144b868401
Matías Speroni <smatias94.rz@gmail.com>
2023-05-28 19:00:01 -0300

    first commit

---
+++ health-service-app/script-mysql/healthservicedb.sql
@@ -0,0 +1,2 @@
+CREATE database healthservicedb;
+USE healthservicedb;
---
+++ health-service-app/src/main/java/com/example/healthserviceapp/HealthServiceAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.healthserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceAppApplication.class, args);
+	}
+}
